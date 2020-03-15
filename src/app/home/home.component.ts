2f00eac85c4b152c689a98644521b9eb0d6f7141
NaeliSilva <naelisilva15@gmail.com>
2020-03-15 19:18:48 -0300

    created home componet

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app_home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  title = 'nae';
+}
